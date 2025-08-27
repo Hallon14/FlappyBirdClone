a6b663c2e7dc5dc8b5a328e56ab2a9c48895583f
Hallon14 <ottowendt@yahoo.se>
2025-08-27 22:08:33 +0200

    Base finished

--- FlappyBirdClone/Assets/Scripts/GameManager.cs
+++ FlappyBirdClone/Assets/Scripts/GameManager.cs
@@ -15,25 +15,39 @@ public class GameManager : MonoBehaviour
 
     public void Awake()
     {
-        
+        Application.targetFrameRate = 60;
+        pause();
+
     }
     public void Play()
     {
-        
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameButton.SetActive(false);
+        overButton.SetActive(false);
+
+        Time.timeScale = 1f;
+        birb.enabled = true;
     }
     public void pause()
     {
-        
+        Time.timeScale = 0f;
+        birb.enabled = false;
     }
 
     public void IncreaseScore()
     {
         score++;
-        scoreText = score.ToString();
+        scoreText.text = score.ToString();
     }
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameButton.SetActive(true);
+        overButton.SetActive(true);
+        pause();
+        playButton.SetActive(true);
     }
 
 }
