65215ad86c91d934996c11c0ce378f01dcc00f84
Hallon14 <ottowendt@yahoo.se>
2025-08-29 11:57:28 +0200

    Did stuff

--- FlappyBirdClone/Assets/Scripts/Pipes.cs
+++ FlappyBirdClone/Assets/Scripts/Pipes.cs
@@ -1,22 +1,29 @@
+using System.Linq;
 using UnityEngine;
 
 public class Pipes : MonoBehaviour
 {
     public float speed = 5f;
     private float leftEdge;
-    public SpriteRenderer spriteRenderer;
+    public Evildooer spawner;
 
     void Start()
     {
-        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x -1f;
+        spawner = FindAnyObjectByType<Evildooer>();
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
     }
 
     void Update()
     {
-        transform.position += Vector3.left * speed * Time.deltaTime;
+        
+    }
+    void LateUpdate()
+    {
+       transform.position += Vector3.left * speed * Time.deltaTime;
         if (transform.position.x < leftEdge)
         {
             Destroy(gameObject);
-        }
+            spawner.activePipes.Remove(gameObject);
+        } 
     }
 }
